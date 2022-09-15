6a1481118ab0f9295fdb68ccedd40416ada89458
ronald fernando maldonado narvaez <saracerquera06@gmail.com>
2022-09-14 22:50:28 -0500

    subiendo proyecto react

---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../componentes/header/Header';
+
+function Home(){
+    return (
+        <div>
+          <Header/>
+        </div>
+    );
+}
+export default Home;
